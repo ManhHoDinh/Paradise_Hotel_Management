a794bb951a6f06151928861f4213749bc7e81ed7
Hồ Đình Mạnh <97241076+ManhHoDinh@users.noreply.github.com>
2023-03-26 23:17:18 +0700

    Done spint 1

--- lib/presentations/screens/splash_screen.dart
+++ lib/presentations/screens/splash_screen.dart
@@ -1,11 +1,9 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:paradise/core/constants/color_palatte.dart';
 import 'package:paradise/core/helpers/local_storage_helper.dart';
 import 'package:paradise/core/helpers/assets_helper.dart';
 import 'package:paradise/core/helpers/text_styles.dart';
-
-import '../../core/models/user_model.dart';
+import 'package:paradise/presentations/screens/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   static String routeName = 'splash_screen';
@@ -25,7 +23,8 @@ class _SplashScreenState extends State<SplashScreen> {
 
   // ignore: non_constant_identifier_names
   void NextIntroScreen() async {
-    await (Future.delayed(Duration(seconds: 2)));
+    await (Future.delayed(Duration(seconds: 3)));
+    Navigator.of(context).pushNamed(LoginScreen.routeName);
     final ignoreIntroScreen =
         LocalStorageHelper.getValue('ignoreIntro') as bool?;
     if (ignoreIntroScreen != null && ignoreIntroScreen) {
@@ -42,13 +41,13 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-        backgroundColor: ColorPalette.greenColor.withOpacity(0.9),
+        backgroundColor: ColorPalette.primaryColor.withOpacity(0.9),
         body: Stack(
           alignment: Alignment.bottomCenter,
           children: [
             Container(
               height: size.height * 1 / 2,
-              color: ColorPalette.greenColor,
+              color: ColorPalette.primaryColor,
             ),
             Container(
               alignment: Alignment.center,
@@ -57,7 +56,7 @@ class _SplashScreenState extends State<SplashScreen> {
                 height: size.width * 4 / 9,
                 width: size.width * 4 / 9,
                 decoration: BoxDecoration(
-                  color: ColorPalette.greenColor,
+                  color: ColorPalette.primaryColor,
                   borderRadius: BorderRadius.circular(size.width * 4 / 9),
                 ),
               ),
@@ -72,7 +71,8 @@ class _SplashScreenState extends State<SplashScreen> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(bottom: size.height * 1 / 3),
+              padding: EdgeInsets.only(
+                  bottom: size.height * 1 / 2 - size.width * 2 / 7),
               child: Text(
                 'Paradise',
                 style: TextStyles.slo.copyWith(shadows: [
