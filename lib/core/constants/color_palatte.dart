407ee36cc3dcbcc814a9190e13ef4fb14ead92ae
TuongPhamCT <tuongpt12503@gmail.com>
2023-04-14 11:47:54 +0700

    temp staff detail

--- lib/core/constants/color_palatte.dart
+++ lib/core/constants/color_palatte.dart
@@ -10,6 +10,9 @@ class ColorPalette {
   static const Color yellowColor = Color(0xffBFBFBF);
   static const Color orangeColor = Color(0xffBFBFBF);
   static const Color calendarGround = Color(0xff002270);
+  static const Color detailBorder = Color(0xff9B9B9B);
+  static const Color rankText = Color(0xff808080);
+  static const Color infoDetail = Color(0xffA8D7D8);
 }
 
 // Color(0xff+x) | Mã hex bỏ # ví dụ #123410 => Color(0xFF123410)
