9a745c131e5712565c6bc8528c6db70db4b61f10
TuongPhamCT <tuongpt12503@gmail.com>
2023-06-04 21:19:43 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class _MyAppState extends State<MyApp> {
         primaryColor: ColorPalette.primaryColor,
         scaffoldBackgroundColor: ColorPalette.backgroundColor,
       ),
-      home: NotificationScreen(),
+      home: SplashScreen(),
       routes: routes,
       debugShowCheckedModeBanner: false,
     );
